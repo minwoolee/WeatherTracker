9507670d8f17fe24c3a2c52cd3534ffa412ae89e
Min Lee <minwoolee@gmail.com>
2025-01-16 21:25:07 -0800

    Additional files

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Min Woo Lee on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
